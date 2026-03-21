7f54bdf467a3931c4e17cf7dd80926d39c8199d5
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-21 13:37:32 +0100

    carousel fix

--- app/components/Testimonial.vue
+++ app/components/Testimonial.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+
+const nextIcon = ref('i-lucide-chevron-right')
+const prevIcon = ref('i-lucide-chevron-left')
 const items = ref([
   {
     name: 'Aisha Bello',
     title: 'Freelancer',
-    description: 'RovelSub Point is the perfect choice for individuals, whether you need to buy data, recharge airtime, or pay bills, our platform makes transactions fast, secure, and stress-free.',
+    description: 'Paying for my TV subscription and electricity bills has never been easier. The platform is intuitive and very user-friendly.',
     icon: 'i-lucide-smile',
   },
   {
@@ -15,7 +18,7 @@ const items = ref([
   {
     name: 'Chukwuemeka Nwosu',
     title: 'RovelSub Agent',
-    description: 'RovelSub Express is the perfect choice for individuals, whether you need to renew subscriptions, recharge your phone, or pay bills, our platform makes transactions quick and hassle-free.',
+    description: 'Becoming a RovelSub agent has been a game-changer. I earn extra income while providing my customers with fast and reliable services.',
     icon: 'i-lucide-globe',
   },
   {
@@ -33,30 +36,34 @@ const items = ref([
       <UCarousel
         v-slot="item"
         loop
-        arrows
         dots
         :autoplay="{ delay: 2000 }"
         wheel-gestures
         :prev="{ variant: 'solid' }"
         :next="{ variant: 'solid' }"
+        prev-icon: prevIcon
+        next-icon: nextIcon
         :items="items"
         :ui="{
-          item: 'ps-0',
-          prev: 'sm:start-8',
-          next: 'sm:end-8',
+          item: 'ps-0 sm:basis-1/2',
+          prev: '-top-10 left-10',
+          next: ' top-0 right-10',
           container: 'ms-0',
-          dots: 'border sm:justify-start',
-          dot: 'data-[state=active]:bg-[#053169]',
+          controls: '',
+          dots: 'sm:justify-start',
+          dot: 'data-[state=active]:bg-[#1177FE]',
 
         }"
       >
-        <div class="border p-4 rounded-sm bg-white">
-          <div class="relative">
-            <span class="border"><UIcon :name="item.item.icon" /></span>
-            <div class="border p-2">
-              <h4>{{ item.item.name }}</h4>
-              <h5>{{ item.item.title }}</h5>
-              <p>{{ item.item.description }}</p>
+        <div class="pt-8 rounded-sm bg-white relative">
+          <span class="absolute top-0 w-24 h-24 rounded-full z-20 bg-white right-10"><NuxtImg src="/images/landing-page/testimonial-img.png" /></span>
+          <div class="relative bg-[#F2FBFF] rounded-lg">
+            <div class="font-poppins py-6 px-4 pb-18">
+              <h4 class="leading-7.5 md:text-[24px] font-bold text-[#4D5155]">{{ item.item.name }}</h4>
+              <h5 class="md:text-[18px] font-normal text-[#1A1F24] leading-[150%]">{{ item.item.title }}</h5>
+              <USeparator class="w-50" />
+              <p class="mt-2 tracking-[5%] text-[#34383D] leading-[190%] text-[16px] font-normal">{{ item.item.description }}</p>
+              <NuxtImg src="/images/landing-page/quote.svg" />
             </div>
           </div>
         </div>
