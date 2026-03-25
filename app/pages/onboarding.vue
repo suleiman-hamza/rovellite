24e67c54888bb092ca1bd32eb479a8890bff6fae
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-25 07:55:04 +0100

    feat/wip: lgon, onboarding and register pages added, configured hero button to match onboarding or register with screen sizes

---
+++ app/pages/onboarding.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section>
+    <div class="border">
+      <UButton label="Login" />
+      <UButton label="Register" />
+    </div>
+  </section>
+</template>
---
+++ app/pages/register.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section>
+    <p>Welcome to Signup page</p>
+  </section>
+</template>
