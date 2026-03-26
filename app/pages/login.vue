6b128dee460a2cd9c3c475da6f760b6efd496b79
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-26 12:07:56 +0100

    chore: login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -5,7 +5,24 @@ definePageMeta({
 </script>
 
 <template>
-  <section>
-    <p>Welcome to login page</p>
+  <section class="grid grid-cols-2">
+    <div class="border flex max-w-md mx-auto my-12">
+      <p>Form stuff</p>
+    </div>
+    <div class="border bg-[#1177FE] min-h-screen relative flex items-center justify-center p-4">
+      <NuxtImg src="/images/top-left.svg" class="absolute top-0 left-0 z-1" alt="ellipse top" />
+      <NuxtImg src="/images/bottom-right.svg" class="absolute bottom-0 right-0 z-0" alt="ellipse bottom" />
+      <div class="border font-poppins text-center flex flex-col gap-5 items-center justify-center">
+        <NuxtImg src="/images/onboard-hero.png" width="403" height="451" alt="happy rovelsub user" />
+        <div>
+          <h3 class="font-bold text-2xl text-white leading-[38.17px]">
+            Welcome To RovelSub Point
+          </h3>
+          <p class="text-[14px] text-white leading-[150%]">
+            The home of data, airtime,  premium subscriptions, and easy bill payment.
+          </p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
