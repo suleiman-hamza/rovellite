f23b1c38e0e81888b602b16298a1d4d41bbab188
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-28 16:26:23 +0100

    Feat: dashboard ui with sidebar

--- app/layouts/dashboard-layout.vue
+++ app/layouts/dashboard-layout.vue
@@ -1,20 +1,67 @@
 <script setup lang="ts">
-// import DashboardHeader from '~/components/DashboardHeader.vue'
-// import DashboardSidebar from '~/components/DashboardSidebar.vue'
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[] = [{
+  label: 'Dashboard',
+  icon: 'i-lucide-house',
+  to: '/app/dashboard',
+}, {
+  label: 'Inbox',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'Contacts',
+  icon: 'i-lucide-users',
+}, {
+  label: 'Settings',
+  icon: 'i-lucide-settings',
+  to: '/app/settings',
+}]
 </script>
 
 <template>
-  <section class="flex items-start w-full h-screen">
-  <!--
-    <DashboardSidebar class="hidden sm:flex w-[350px] h-screen overflow-auto" />
-    <main class="h-screen w-full flex flex-col">
-      <DashboardHeader />
-      <section class="flex-1 w-full h-full overflow-auto">
+  <UDashboardGroup>
+    <UDashboardSidebar collapsible resizable :ui="{ footer: 'border-t border-default', header: 'border', body: 'px-0 py-0' }">
+      <template #header="{ collapsed }">
+        <div v-if="!collapsed" class="w-auto shrink-0 border">
+          <NuxtImg
+            loading="eager"
+            src="/images/landing-page/rovel-new-logo.svg"
+            width="100"
+            height="100"
+            alt="Rovelsub point header logo"
+            class=""
+          />
+        </div>
+        <UIcon v-else name="i-simple-icons-nuxtdotjs" class="size-5 text-primary mx-auto" />
+      </template>
+
+      <template #default="{ collapsed }">
+        <UNavigationMenu
+          :collapsed="collapsed"
+          :items="items"
+          orientation="vertical"
+          :ui="{ link: 'before:bg-transparent before:rounded-none', linkLeadingIcon: 'text-secondary data-active:border', linkLabel: '', item: '' }"
+        />
+      </template>
+
+      <template #footer="{ collapsed }">
+        <UButton :label="collapsed ? undefined : 'Log Out'" icon="i-lucide-log-out" variant="ghost" :ui="{ label: 'text-[#676A6D]', leadingIcon: 'text-[#676A6D]' }" class="w-full justify-center" />
+      </template>
+    </UDashboardSidebar>
+    <UDashboardPanel resizable>
+      <template #header>
+        <UDashboardNavbar title="Welcome Back Rovel">
+          <template #leading>
+            <UDashboardSidebarCollapse />
+          </template>
+        </UDashboardNavbar>
+      </template>
+
+      <template #body>
         <slot />
-      </section>
-    </main>
-    -->
-  </section>
+      </template>
+    </UDashboardPanel>
+  </UDashboardGroup>
 </template>
 
 <style scoped></style>
---
+++ app/pages/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard-layout',
+  // middleware: 'auth',
+})
+</script>
+
+<template>
+  <NuxtPage />
+</template>
