49b5c0c57beff2b404bbd346c945ea812fb66dcd
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-30 13:38:01 +0100

    feat: dashboard sidebar

--- app/layouts/dashboard-layout.vue
+++ app/layouts/dashboard-layout.vue
@@ -6,11 +6,48 @@ const items: NavigationMenuItem[] = [{
   icon: 'i-lucide-house',
   to: '/app/dashboard',
 }, {
-  label: 'Inbox',
+  label: 'Subscriptions',
   icon: 'i-lucide-inbox',
 }, {
-  label: 'Contacts',
+  label: 'eSim',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'Gift cards',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'Data/Internet',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'Airtime',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'TV/Decoders',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'Betting',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'eSim',
+  icon: 'i-lucide-inbox',
+}, {
+  label: 'Electricity',
   icon: 'i-lucide-users',
+}, {
+  label: 'Education',
+  icon: 'i-lucide-settings',
+  to: '/',
+}, {
+  label: 'Solar System',
+  icon: 'i-lucide-settings',
+  to: '/',
+}, {
+  label: 'Fund wallet',
+  icon: 'i-lucide-settings',
+  to: '/',
+}, {
+  label: 'Transactions',
+  icon: 'i-lucide-settings',
+  to: '/',
 }, {
   label: 'Settings',
   icon: 'i-lucide-settings',
@@ -19,29 +56,37 @@ const items: NavigationMenuItem[] = [{
 </script>
 
 <template>
-  <UDashboardGroup>
-    <UDashboardSidebar collapsible resizable :ui="{ footer: 'border-t border-default', header: 'border', body: 'px-0 py-0' }">
+  <UDashboardGroup class="bg-[#F9F9FB]">
+    <UDashboardSidebar collapsible resizable :ui="{ root: 'rounded-r-[16px] bg-white', footer: 'border-t border-default', header: 'flex items-center shrink justify-center p-1', body: 'px-0 py-0' }">
       <template #header="{ collapsed }">
-        <div v-if="!collapsed" class="w-auto shrink-0 border">
+        <div v-if="!collapsed" class="w-auto border h-(--ui-header-height) flex items-center justify-center py-2">
           <NuxtImg
             loading="eager"
             src="/images/landing-page/rovel-new-logo.svg"
             width="100"
             height="100"
             alt="Rovelsub point header logo"
-            class=""
+            class="w-auto h-20 mx-auto"
           />
         </div>
-        <UIcon v-else name="i-simple-icons-nuxtdotjs" class="size-5 text-primary mx-auto" />
       </template>
-
       <template #default="{ collapsed }">
-        <UNavigationMenu
+        <nav class="border">
+          <ul>
+            <li v-for="item in items" :key="item.label">
+              <NuxtLink :to="item.to" class="flex items-center p-2 text-sm font-medium text-gray-700 truncate">
+                <UIcon :name="item.icon" class="mr-2" />
+                {{ item.label }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+        <!-- <UNavigationMenu
           :collapsed="collapsed"
           :items="items"
           orientation="vertical"
           :ui="{ link: 'before:bg-transparent before:rounded-none', linkLeadingIcon: 'text-secondary data-active:border', linkLabel: 'border', item: '' }"
-        />
+        /> -->
       </template>
 
       <template #footer="{ collapsed }">
@@ -64,4 +109,13 @@ const items: NavigationMenuItem[] = [{
   </UDashboardGroup>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  scrollbar-width: thin;
+}
+.router-link-active {
+  font-weight: 900;
+  color: #34383D;
+  background-color: #F2FBFF;
+}
+</style>
--- app/pages/app/dashboard/index.vue
+++ app/pages/app/dashboard/index.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const cards = ref([
+  {
+    title: 'Icons',
+  },
+  {
+    title: 'Fonts',
+  },
+  {
+    title: 'Color Mode',
+  },
+])
+const cards2 = ref([
+  {
+    title: 'Icons',
+  },
+  {
+    title: 'Fonts',
+  },
+  {
+    title: 'Color Mode',
+  },
+  {
+    title: 'Color',
+  },
+])
+</script>
 
 <template>
-  <p>Dashbaord Page</p>
+  <main>
+    <UPageGrid class="bg-[#1177FE] p-4">
+      <UPageCard
+        v-for="(card, index) in cards"
+        :key="index"
+        v-bind="card"
+      />
+    </UPageGrid>
+    <UPageGrid :ui="{ base: 'lg:grid-cols-4' }" class="bg-[#FFFFFF] p-2">
+      <UPageCard
+        v-for="(card, index) in cards2"
+        :key="index"
+        v-bind="card"
+        :ui="{ container: 'bg-[#F9F9FB]' }"
+      />
+    </UPageGrid>
+  </main>
 </template>
