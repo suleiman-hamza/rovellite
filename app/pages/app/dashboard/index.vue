a3731a23e5708ee8fdc72360139c904764a7adb6
suleiman hamza <hamzasuleiman530@gmail.com>
2026-04-01 00:05:58 +0100

    style: added icons for wallet functionsin dashboard

--- app/pages/app/dashboard/index.vue
+++ app/pages/app/dashboard/index.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 const cards = ref([
   {
-    title: 'Icons',
+    title: 'Wallet Balance',
+    icon: '/images/icons/wallet-ballance.svg',
   },
   {
-    title: 'Fonts',
+    title: 'Wallet In',
+    icon: '/images/icons/wallet-in.svg',
   },
   {
-    title: 'Color Mode',
+    title: 'Wallet Out',
+    icon: '/images/icons/wallet-out.svg',
   },
 ])
 const cards2 = ref([
@@ -32,8 +35,26 @@ const cards2 = ref([
       <UPageCard
         v-for="(card, index) in cards"
         :key="index"
-        v-bind="card"
-      />
+        :ui="{ container: 'p-0 sm:p-0' }"
+      >
+        <template #default>
+          <div class="bg-white p-4 rounded-lg">
+            <div class="border flex gap-1 items-center">
+              <span class="bg-[#1177FE] p-2">
+                <NuxtImg :src="card.icon" :alt="card.title" class="w-10 h-10" />
+              </span>
+              <div class="border">
+                <h3 class="text-[14px] text-[#1177FE] font-medium">
+                  {{ card.title }}
+                </h3>
+                <p class="text-[20px] font-bold">
+                  N0.00
+                </p>
+              </div>
+            </div>
+          </div>
+        </template>
+      </UPageCard>
     </UPageGrid>
     <UPageGrid :ui="{ base: 'lg:grid-cols-4' }" class="bg-[#FFFFFF] p-2">
       <UPageCard
