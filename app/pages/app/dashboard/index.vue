8743b42917bb4a5e84b72f74162a4e0489558c9d
suleiman hamza <hamzasuleiman530@gmail.com>
2026-04-02 18:17:29 +0100

    feat: palmpay ui customization

--- app/pages/app/dashboard/index.vue
+++ app/pages/app/dashboard/index.vue
@@ -13,6 +13,8 @@ const cards = ref([
     icon: '/images/icons/wallet-out.svg',
   },
 ])
+
+const accountNumber = ref('1234567890')
 </script>
 
 <template>
@@ -26,26 +28,46 @@ const cards = ref([
         >
           <template #default>
             <div class="bg-white p-4 rounded-lg">
-              <div class="flex gap-1 items-center">
-                <span class="bg-[#1177FE] p-2">
-                  <NuxtImg :src="card.icon" :alt="card.title" class="w-10 h-10" />
+              <div class="flex gap-2 items-center">
+                <span class="bg-[#1177FE] p-2.5 rounded-sm">
+                  <NuxtImg :src="card.icon" :alt="card.title" class="w-8 h-8" />
                 </span>
-                <div class="border">
-                  <h3 class="text-[14px] text-[#1177FE] font-medium">
-                    {{ card.title }}
-                  </h3>
-                  <p class="text-[20px] font-bold">
+                <div class="">
+                  <p class="text-[20px] font-bold text-[#4D5155]">
                     N0.00
                   </p>
+                  <h3 class="text-[14px] md:text-[18px] tracking-[2%] md:tracking-[10%] text-[#676A6D] font-medium">
+                    {{ card.title }}
+                  </h3>
                 </div>
               </div>
             </div>
           </template>
         </UPageCard>
       </UPageGrid>
-      <div class="flex items-center justify-between p-4 sm:px-6">
-        <p>You do not have a wallet</p>
-        <UButton label="Create Wallet" size="md" :ui="{ label: 'text-white text-[18px] md:text-[24px]' }" />
+      <!-- show create wallet option for users without a wallet -->
+
+      <div class="flex md:flex-row flex-col items-center justify-between p-4 sm:px-6">
+        <p class="font-bold text-[18px] md:text-[24px]">
+          You do not have a wallet
+        </p>
+        <UButton label="Create Wallet" size="lg" :ui="{ label: 'text-white text-[16px] md:text-[20px]', base: 'bg-[#1177FE] px-6' }" />
+      </div>
+
+      <!-- show palmpay wallet for users with a wallet -->
+      <div class="flex md:flex-row flex-col gap-4 md:gap-0 items-center justify-between p-4 sm:px-6">
+        <div class="flex flex-col gap-3 text-center">
+          <div class="flex gap-2 md:gap-3 items-center font-bold text-[18px] md:text-[24px]">
+            <NuxtImg src="/images/icons/palmpay-wallet.svg" alt="palmpay wallet" class="w-8 h-8" />
+            <h3>Palmpay</h3>
+            <USeparator orientation="vertical" class="h-4" />
+            <span>{{ accountNumber }}</span>
+          </div>
+          <p class="tracking-[2%] text-[#3A3A3A] leading-[150%] font-normal">
+            Transfer to Fund wallet. #50 charge applies
+          </p>
+        </div>
+        <UButton label="Copy" size="lg" :ui="{ label: 'text-white text-[16px] md:text-[20px]', base: 'bg-[#1177FE] px-6' }" />
       </div>
     </section>
 
@@ -69,10 +91,12 @@ const cards = ref([
     <section class="mt-4 rounded-lg">
       <UPageGrid :ui="{ base: '' }" class="bg-[#FFFFFF] p-4 sm:px-6 gap-4 rounded-lg md:grid-cols-4 lg:grid-cols-5">
         <div v-for="(_, index) in 10" :key="index" class="border border-[#DBF4FF] rounded-lg flex flex-col justify-center items-center p-4">
-          <span class="bg-[#DBF4FF] p-2">
+          <span class="bg-[#DBF4FF] p-2 px-4 rounded-lg">
             <NuxtImg src="/images/icons/airtime.svg" alt="come svg for pool" class="w-10 h-10" />
           </span>
-          <h4>Referal Bonus</h4>
+          <h4 class="tracking-[5%] text-[16px] text-[#676A6D] font-normal">
+            Referal Bonus
+          </h4>
         </div>
       </UPageGrid>
     </section>
