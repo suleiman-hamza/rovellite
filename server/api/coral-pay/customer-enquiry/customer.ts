6a989447985ec233c642f0e2720f427001caec6a
suleiman hamza <hamzasuleiman530@gmail.com>
2026-04-02 08:08:56 +0100

    added betting images, configured the coral pay route to sync with the betting api

---
+++ server/api/coral-pay/customer-enquiry/customer.ts
@@ -0,0 +1,19 @@
+// import type { CustomerAccountDetails } from "~/shared/types/biller-types";
+import Buffer from 'node:buffer'
+
+export default defineEventHandler(async (event) => {
+  const { CORALPAY_USERNAME, CORALPAY_PASSWORD } = useRuntimeConfig()
+  const credentials = Buffer.Buffer.from(`${CORALPAY_USERNAME}:${CORALPAY_PASSWORD}`).toString('base64')
+
+  const payload = await readBody(event)
+  // const payload = { "customerId": 10277062245, "billerSlug": "DSTV", "productName": "COMPACT", "orderId": 6, "billerId": 2 };
+  const response = await $fetch('https://sandbox1.coralpay.com/coralpay-vas/api/transactions/customer-lookup', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${credentials}`,
+      'Content-Type': 'application/json',
+    },
+    body: payload,
+  })
+  return response
+})
