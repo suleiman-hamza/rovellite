aee768a6fc6f34abcffe4f47f391c136fc702908
suleiman hamza <hamzasuleiman530@gmail.com>
2026-04-22 20:59:43 +0100

    fix lint

--- app/pages/app/settings/index.vue
+++ app/pages/app/settings/index.vue
@@ -10,8 +10,22 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-    <h3>Settings</h3>
-    <p>{{ store.userProfile?.email }}</p>
-  </div>
+  <main class="border bg-white rounded-[20px]">
+    <!--header/profile img-->
+    <div class="">
+      <div class="relative rounded-full w-20 h-20 border border-primary">
+        <UUser class="w-full h-full" :avatar="{
+          src: store.userProfile?.avatar_url,
+          loading: 'lazy',
+          icon: 'i-lucide-image'
+        }" />
+        <span class="absolute right-0 bottom-5 bg-white rounded-full shadow">
+          <UFileUpload icon="i-lucide-camera" variant="button" :ui="{ base: 'rounded-full', icon: 'text-primary' }"
+            class="rounded-full" />
+        </span>
+      </div>
+      <span class="bg-[#DBF4FF]"></span>
+      <span class="bg-white"></span>
+    </div>
+  </main>
 </template>
