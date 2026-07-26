95f83ad75ede54cc79d50118180357d9cd71e54c
SULEIMAN HAMZA <hamzasuleiman530@gmail.com>
2026-07-26 11:58:50 +0100

    feat(StatisticalReportChart): update data structure to include new products and enhance chart display

--- app/pages/admin/dashboard/components/StatisticalReportChart.vue
+++ app/pages/admin/dashboard/components/StatisticalReportChart.vue
@@ -6,6 +6,7 @@ interface FinancialRecord {
   revenue: number
   expenses: number
   profit: number
+  new_products: number
 }
 const _props = defineProps<{ data: FinancialRecord[] }>()
 const height = 220
@@ -18,12 +19,13 @@ const y = [
   (d: FinancialRecord) => d.revenue,
   (d: FinancialRecord) => d.expenses,
   (d: FinancialRecord) => d.profit,
+  (d: FinancialRecord) => d.new_products,
 ]
 
-const seriesColors = { revenue: 'blue', expenses: 'red', profit: 'green' }
+const seriesColors = { revenue: 'blue', expenses: 'red', profit: 'green', new_products: 'yellow' }
 
 const items = computed(() => {
-  const keys: ('revenue' | 'expenses' | 'profit')[] = ['revenue', 'expenses', 'profit']
+  const keys: ('revenue' | 'expenses' | 'profit' | 'new_products')[] = ['revenue', 'expenses', 'profit', 'new_products']
   return keys.map(key => ({
     name: key.charAt(0).toUpperCase() + key.slice(1),
     color: seriesColors[key],
@@ -32,18 +34,19 @@ const items = computed(() => {
 </script>
 
 <template>
-  <div class="p-4 border w-fit">
+  <div class="p-4 w-full">
     <p class="text-[#4D5155] font-semibold md:font-bold text-[18px] md:text-[20px] font-poppins tracking-[2%] mb-2">
-      Statistical Report
+      This Week Statistical Report
     </p>
     <VisXYContainer :data="data" :height="height">
       <VisLine :x="x" :y="y[0]" color="blue" />
       <VisLine :x="x" :y="y[1]" color="red" />
       <VisLine :x="x" :y="y[2]" color="green" />
+      <VisLine :x="x" :y="y[3]" color="yellow" />
       <VisAxis type="x" :tick-format="(tick: number) => data[tick]?.month" :num-ticks="data.length" />
       <VisAxis type="y" />
     </VisXYContainer>
-    <div class="mt-2 p-1">
+    <div class="mt-2">
       <VisBulletLegend :items="items" />
     </div>
   </div>
--- app/pages/admin/dashboard/index.vue
+++ app/pages/admin/dashboard/index.vue
@@ -10,32 +10,26 @@ const chartData = ref([
   { name: 'Non active users', value: 40 },
 ])
 
-const data = [
-  { month: 'Jan', revenue: 50, expenses: 30, profit: 20 },
-  { month: 'Feb', revenue: 70, expenses: 44, profit: 26 },
-  { month: 'Mar', revenue: 30, expenses: 50, profit: -20 },
-  { month: 'Apr', revenue: 50, expenses: 45, profit: 5 },
-  { month: 'May', revenue: 20, expenses: 37, profit: -17 },
-  { month: 'Jun', revenue: 10, expenses: 45, profit: -35 },
-  { month: 'Jul', revenue: 60, expenses: 50, profit: 10 },
-  { month: 'Aug', revenue: 80, expenses: 55, profit: 25 },
-  { month: 'Sep', revenue: 40, expenses: 48, profit: -8 },
-  { month: 'Oct', revenue: 90, expenses: 65, profit: 25 },
-  { month: 'Nov', revenue: 75, expenses: 60, profit: 15 },
-  { month: 'Dec', revenue: 100, expenses: 70, profit: 30 },
+const WeeklyStatData = [
+  { month: 'Mon', revenue: 50000, expenses: 30000, profit: 65000, new_products: 20022 },
+  { month: 'Tue', revenue: 70000, expenses: 44000, profit: 26000, new_products: 45000 },
+  { month: 'Wed', revenue: 30000, expenses: 50000, profit: 67000, new_products: 16550 },
+  { month: 'Thur', revenue: 50000, expenses: 45000, profit: 25000, new_products: 25655 },
+  { month: 'Fri', revenue: 20000, expenses: 37000, profit: 17000, new_products: 50000 },
+  { month: 'Sat', revenue: 10000, expenses: 65006, profit: 30000, new_products: 300 },
 ]
 </script>
 
 <template>
   <main class="">
     <MetricCard />
-    <section class="border flex flex-col md:flex-row gap-4 mt-6">
-      <StatisticalReportChart class="bg-white rounded-[20px] w-fit flex-1 shadow-md" :data="data" />
-      <UserDonutChart class="bg-white rounded-[20px] max-w-80 w-fit shadow-md" :data="chartData" />
+    <section class="flex flex-col md:flex-row gap-4 mt-6">
+      <StatisticalReportChart class="bg-white rounded-[20px] w-fit flex-1 shadow-md" :data="WeeklyStatData" />
+      <UserDonutChart class="bg-white rounded-[20px] w-full shadow-md" :data="chartData" />
     </section>
     <section class="flex flex-col md:flex-row gap-4 mt-6">
-      <ProductSales class="bg-white rounded-[20px] w-fit max-w-80" />
-      <TransactionsTable class="bg-white rounded-[20px] w-fit md:flex-1" />
+      <ProductSales class="bg-white rounded-[20px] w-full shadow-md" />
+      <!-- <TransactionsTable class="bg-white rounded-[20px] w-fit md:flex-1" /> -->
     </section>
   </main>
 </template>
